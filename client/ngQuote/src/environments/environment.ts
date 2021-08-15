cee01a31b78bb570ced6f16f02c4ac02865cf435
Mephis28 <sergei.krn@gmail.com>
2021-08-15 14:56:26 +0300

    "fix eror and finished homework 2"

--- client/ngQuote/src/environments/environment.ts
+++ client/ngQuote/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
 
   apiUrls: {
-    quote: 'http://localhost:3000/api', // Put here your QUOTE API BASE URL. E.g.: api or https://quote.com/api
+    quote: 'http://localhost:5555/api', // Put here your QUOTE API BASE URL. E.g.: api or https://quote.com/api
     share: '', // Put here your SHARE API BASE URL. E.g.: api or https://share.com/api
   },
 };
