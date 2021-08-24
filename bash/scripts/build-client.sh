5547e0505427948722ab1bd9fb4e074b44b9b6a2
Mephis29 <asdfasdasdasasd>
2021-08-24 11:42:00 +0300

    feat: add build-client.sh

--- bash/scripts/build-client.sh
+++ bash/scripts/build-client.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
-zip -q -r client-app.zip ../bash/
+cd ../../client/ngQuote
+npm run build
+if [[ -e ./dist/client-app.zip ]]; then
+    rm ./dist/client-app.zip
+    zip -q -r ./dist/client-app.zip dist
+  else
+    zip -q -r ./dist/client-app.zip dist
+fi
