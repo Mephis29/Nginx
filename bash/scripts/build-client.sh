0aa5ba83eb217b420c38a3414733892d9ff80ed1
Mephis29 <asdfasdasdasasd>
2021-10-12 12:04:08 +0300

    delete and copy only app

--- bash/scripts/build-client.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-cd ../../client/ngQuote/dist
-#npm run build
-if [[ -e ./client-app.zip ]]; then
-    rm ./client-app.zip
-fi
-zip -q -r ./client-app.zip ./
--- src/app/environment.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    // APP_PORT: 3000 || process.env.PORT,
-    APP_PORT: 5555,
-    NODE_ENV: 'Development',
-    ENV_CONFIGURATION: 'Sergei configuration',
-  };
